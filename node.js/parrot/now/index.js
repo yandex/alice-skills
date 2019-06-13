d7e2e8b332bd55ceadc474a79e7944328c71898b
ogorbacheva <ogorbacheva@yandex-team.ru>
2019-06-13 14:36:08 +0300

    Reformat code2

--- node.js/parrot/now/index.js
+++ node.js/parrot/now/index.js
@@ -1,11 +1,11 @@
 // Для асинхронной работы используется пакет micro.
-const {json} = require('micro');
+const { json } = require('micro');
 
 // Запуск асинхронного сервиса.
 module.exports = async (req, res) => {
 
     // Из запроса извлекаются свойства request, session и version.
-    const {request, session, version} = await json(req);
+    const { request, session, version } = await json(req);
 
     // В тело ответа вставляются свойства version и session из запроса.
     // Подробнее о формате запроса и ответа — в разделе Протокол работы навыка.
