f49420dd0454cf77ba1116d4189eabf95db53910
ogorbacheva <ogorbacheva@yandex-team.ru>
2019-06-13 14:31:17 +0300

    Some fixes for quick-start

--- node.js/parrot/now/index.js
+++ node.js/parrot/now/index.js
@@ -1,27 +1,28 @@
 // Для асинхронной работы используется пакет micro.
-const { json } = require('micro');
+const {json} = require('micro');
 
 // Запуск асинхронного сервиса.
-module.exports = async req => {
+module.exports = async (req, res) => {
 
-  // Из запроса извлекаются свойства request, session и version.
-  const { request, session, version } = await json(req);
+    // Из запроса извлекаются свойства request, session и version.
+    const {request, session, version} = await json(req);
 
-  // В тело ответа вставляются свойства version и session из запроса.
-  // Подробнее о формате запроса и ответа — в разделе Протокол работы навыка.
-  return {
-    version,
-    session,
-    response: {
+    // В тело ответа вставляются свойства version и session из запроса.
+    // Подробнее о формате запроса и ответа — в разделе Протокол работы навыка.
+    res.end(JSON.stringify({
+            version,
+            session,
+            response: {
 
-      // В свойстве response.text возвращается исходная реплика пользователя.
-      // Если навык был активирован без дополнительной команды,
-      // пользователю нужно сказать "Hello!".
-      text: request.original_utterance || 'Hello!',
+                // В свойстве response.text возвращается исходная реплика пользователя.
+                // Если навык был активирован без дополнительной команды,
+                // пользователю нужно сказать "Hello!".
+                text: request.original_utterance || 'Hello!',
 
-      // Свойство response.end_session возвращается со значением false,
-      // чтобы диалог не завершался.
-      end_session: false,
-    },
-  };
+                // Свойство response.end_session возвращается со значением false,
+                // чтобы диалог не завершался.
+                end_session: false,
+            },
+        })
+    );
 };
